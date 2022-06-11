9126d7654045e316992dff95e6155b297a931475
Liwei <liliwei25@hotmail.com>
2022-06-12 01:19:00 +0800

    Setup nest config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { resolve } from 'path';
 import { PrismaModule } from 'nestjs-prisma';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GLOBAL_PREFIX } from '../main';
+
+const STATIC_ROOT_PATH_KEY = 'SERVE_STATIC_ROOT_PATH';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: resolve(process.env.SERVE_STATIC_ROOT_PATH),
-      exclude: ['/api*'],
+    ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => [
+        {
+          rootPath: resolve(
+            configService.getOrThrow<string>(STATIC_ROOT_PATH_KEY)
+          ),
+          exclude: [`/${GLOBAL_PREFIX}*`],
+        },
+      ],
     }),
-    PrismaModule.forRoot(),
+    PrismaModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,24 +3,36 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 import { PrismaService } from 'nestjs-prisma';
+import { ConfigService } from '@nestjs/config';
+
+const PATH_KEY = 'PORT';
+export const GLOBAL_PREFIX = 'api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix(GLOBAL_PREFIX);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  const port = process.env.PORT || 3333;
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get(PATH_KEY) || 3333;
+
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
   );
 }
 
