4238b7684f955f1cb9af8c171ba95c2da9ef8dc5
Liwei <liliwei25@hotmail.com>
2022-06-11 22:16:44 +0800

    Setup ServeStatic to serve admin-ui

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(process.env.SERVE_STATIC_ROOT_PATH),
+      exclude: ['/api*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
