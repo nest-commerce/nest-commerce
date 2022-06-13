69f35e8ad05394324189deefb15381014b9a9e6f
Liwei <liliwei25@hotmail.com>
2022-06-13 17:54:03 +0800

    Setup swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import {
   ClassSerializerInterceptor,
+  INestApplication,
   Logger,
   ValidationPipe,
 } from '@nestjs/common';
@@ -13,17 +14,18 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { PrismaService } from 'nestjs-prisma';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PATH_KEY = 'PORT';
-export const GLOBAL_PREFIX = 'api';
+export const API_GLOBAL_PREFIX = 'api';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(GLOBAL_PREFIX);
-
+async function initPrisma(app: INestApplication) {
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+}
 
+async function initGlobalConfigs(app: INestApplication) {
+  app.setGlobalPrefix(API_GLOBAL_PREFIX);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -32,14 +34,35 @@ async function bootstrap() {
     })
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+}
 
+async function initSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Nest Commerce API')
+    .setDescription('Nest Commerce API')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${API_GLOBAL_PREFIX}/swagger`, app, document);
+}
+
+async function startApp(app: INestApplication) {
   const configService = app.get(ConfigService);
-  const port = configService.get(PATH_KEY) || 3333;
+  const port = configService.get(PATH_KEY) || 3000;
 
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await Promise.all([
+    initPrisma(app),
+    initGlobalConfigs(app),
+    initSwagger(app),
+  ]);
+  await startApp(app);
 }
 
 bootstrap();
